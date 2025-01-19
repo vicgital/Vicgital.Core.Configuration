c1619a286d3025544329d15b7234bb5acd44a8be
Vicgital <Vicgital@hotmail.com>
2025-01-19 16:40:47 -0600

    Initial Commit

---
+++ src/Vicgital.Core.Configuration/Services/AppConfigurationService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Vicgital.Core.Configuration.Services
+{
+    public class AppConfigurationService(IConfiguration config) : IAppConfigurationService
+    {
+        private readonly IConfiguration _config = config;
+
+        public string GetValue(string key)
+        {
+            var value = _config[key] ?? throw new ArgumentException($"{key} was not found in App Configuration");
+            return value;
+        }
+
+        public string GetValue(string key, string defaultValue)
+        {
+            var value = _config[key] ?? defaultValue;
+            return value;
+        }
+    }
+}
